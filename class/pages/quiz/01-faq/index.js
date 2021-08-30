f660a1b0ffaa0671f59a422097d7e8137cab2ec3
오수향 <suhyang@osuhyangs-MacBook-Pro.local>
2021-08-30 18:50:10 +0900

    updatemy

--- class/pages/quiz/01-faq/index.js
+++ class/pages/quiz/01-faq/index.js
@@ -1,8 +1,70 @@
-import {} from '../../styles/Quiz01-faq'
+import {Line, ItemWrapper, Itemdiv, Numberdiv, Questiondiv, Statusbar, Wrapper} from '../../../styles/Quiz01-faq'
 
 export default function Quiz01FAQPage() {
-
     return(
+        <Wrapper>
+            <Statusbar></Statusbar>
+            <Line></Line>
+            <ItemWrapper>
+            <Itemdiv>
+                <Numberdiv>
+                    Q. Q1
+                </Numberdiv>
+                <Questiondiv>
+                    리뷰 작성은 어떻게 하나요?
+                </Questiondiv>
+            </Itemdiv>
 
+            <Itemdiv>
+                <Numberdiv>
+                    Q. Q2
+                </Numberdiv>
+                <Questiondiv>
+                    리뷰 수정/삭제는 어떻게 하나요?
+                </Questiondiv>
+            </Itemdiv>
+
+            <Itemdiv>
+                <Numberdiv>
+                    Q. Q3
+                </Numberdiv>
+                <Questiondiv>
+                    아이디/비밀번호를 잊어버렸어요.
+                </Questiondiv>
+            </Itemdiv>
+
+            <Itemdiv>
+                <Numberdiv>
+                    Q. Q4
+                </Numberdiv>
+                <Questiondiv>
+                    회원탈퇴를 하고싶어요.
+                </Questiondiv>
+            </Itemdiv>
+
+            <Itemdiv>
+                <Numberdiv>
+                    Q. Q5
+                </Numberdiv>
+                <Questiondiv>
+                    출발지 설정은 어떻게 하나요?
+                </Questiondiv>
+            </Itemdiv>
+
+            <Itemdiv>
+                <Numberdiv>
+                    Q. Q6
+                </Numberdiv>
+                <Questiondiv>
+                    비밀번호를 변경하고 싶어요.
+                </Questiondiv>
+            </Itemdiv>
+
+            </ItemWrapper>
+            
+
+
+
+        </Wrapper>
     )
 }
--- class/styles/Quiz01-faq.js
+++ class/styles/Quiz01-faq.js
@@ -2,9 +2,90 @@ import styled from '@emotion/styled'
 
 export const Wrapper = styled.div`
     background-color: skyblue;
-    width: 300px;
-    height: 300px;
+    width: 640px;
+    height: 1138px;
+    display: flex;
+    background-color: #fff;
+    flex-direction: column;
+    justify-content: space-around;
+    border: 1px solid gray;
+    border-radius: 5px;
+    padding: 50px;
+    position: fixed;
+    top: 100px;
+    left: 100px;
+    right: 0;
+`
+export const ItemWrapper = styled.div`
+    position: fixed;
+    top: 475px;
+    left: 120px;
+    width: 640px;
+    height: 694px;
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: space-between;
+    padding: 20px;
+    `
+
+export const Statusbar = styled.div`
+    position: fixed;
+    top: 100px;
+    left: 100px;
+    right: 0;
+    width: 640px;
+    height: 43px;
+    margin: 0 0 36px;
+    padding: 5px 13px 7px 445px;
+    background-color: rgba(0, 0, 0, 0.1);
+}
+`
+export const Itemdiv = styled.span`
+    
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`
+export const Numberdiv = styled.span`
+    width: 490px;
+    height: 20px;
+    margin: 0 0 14px;
+    font-family: SpoqaHanSans;
+    font-size: 18px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.19;
+    letter-spacing: normal;
+    color: #adadad; 
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`
+export const Questiondiv = styled.span`
+    width: 490px;
+    height: 26px;
+    margin: 14px 0 0;
+    font-family: SpoqaHanSans;
+    font-size: 24px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.58;
+    letter-spacing: normal;
+    color: #444;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`
+
+export const Line = styled.div`
+    position: fixed;
+    top: 446px;
+    left: 100px;
+    right: 0;
+    width: 640px;
+    height: 1px;
+    margin: 50px 0 29px;
+    border: solid 1px #cacaca;
 `
